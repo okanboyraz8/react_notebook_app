9ecf3a2c73e5a2a788e6f019e2c8d546e979fa7b
Okan Boyraz <134229327+okanboyraz8@users.noreply.github.com>
2023-08-03 22:52:55 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,7 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6zxKBRnVAXbll6kHg8S9HkJZQOfYVHjQ",
-    authDomain: "notebook-app-b3422.firebaseapp.com",
-    projectId: "notebook-app-b3422",
-    storageBucket: "notebook-app-b3422.appspot.com",
-    messagingSenderId: "64447473881",
-    appId: "1:64447473881:web:a2907f0e7ebf10c4f9f6c8"
+    //firebase config file
   };
 
 const app = initializeApp(firebaseConfig);
